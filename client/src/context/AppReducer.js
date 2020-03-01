caf4e9db23744555adb1a8f1daea71b39eb2ffa6
Sean Parkin <sdparkin@gmail.com>
2020-03-01 09:42:21 -0600

    Removed context files

--- client/src/context/AppReducer.js
+++
@@ -1,9 +0,0 @@
-export default (state, action) => {
-  switch (action.type) {
-    case 'GET_NOTES':
-      return { ...state, notes: action.payload };
-
-    default:
-      return state;
-  }
-};
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -22,6 +23,10 @@ app.use(cors());
 const notes = require('./routes/notes');
 
 app.use('/api/v1/notes', notes);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
