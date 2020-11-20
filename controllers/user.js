833adb2b9c1d949747bbedc9c25509905ef3de01
minnecoder <sdparkin@protonmail.com>
2020-11-20 01:07:49 -0600

    Added addUser

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,43 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
+// @desc Add user
+// @route /user/add
+// @access Verified User
+
+exports.addUser = async (req, res) => {
+  // Check if user is already used
+  const userExists = await User.findOne({ userName: req.body.userName });
+  if (userExists)
+    return res.status(400).json({ error: "User name already exists" });
+
+  // Check if email is already used
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists)
+    return res.status(400).json({ error: "Email already exists" });
+
+  // Create hashed password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  try {
+    const user = await User.create({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: hashedPassword,
+      role: req.body.role,
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server Error" });
+  }
+};
+
 // @desc Login in user
 // @route POST /user/login
 // @access Driver, Admin
@@ -9,7 +46,9 @@ const User = require("../models/User");
 exports.loginUser = async (req, res) => {
   // Check if user exists
   const user = await User.findOne({ userName: req.body.userName });
-  if (!user) return res.json({ error: "User name or password is wrong" });
+  if (!user) {
+    return res.status(400).json({ error: "User name or password is wrong" });
+  }
 
   // Check if password is correct
   const validPassword = await bcrypt.compare(req.body.password, user.password);
@@ -22,5 +61,5 @@ exports.loginUser = async (req, res) => {
     { _id: user._id, role: user.role },
     process.env.JWT_SECRET
   );
-  res.header("Authorization", token).json({ token });
+  return res.header("Authorization", token).json({ token });
 };
