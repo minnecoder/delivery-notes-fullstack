59028786ec5835fd09394b554ea8dc62b121f71a
minnecoder <sdparkin@protonmail.com>
2020-11-20 22:56:27 -0600

    Removed email from user controller

--- controllers/user.js
+++ controllers/user.js
@@ -12,11 +12,6 @@ exports.addUser = async (req, res) => {
   if (userExists)
     return res.status(400).json({ error: "User name already exists" });
 
-  // Check if email is already used
-  const emailExists = await User.findOne({ email: req.body.email });
-  if (emailExists)
-    return res.status(400).json({ error: "Email already exists" });
-
   // Create hashed password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -24,7 +19,6 @@ exports.addUser = async (req, res) => {
   try {
     const user = await User.create({
       userName: req.body.userName,
-      email: req.body.email,
       password: hashedPassword,
       role: req.body.role,
     });
