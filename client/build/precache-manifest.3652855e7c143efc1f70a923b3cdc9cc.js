af919596f914899f763dabd264ea3721415f195c
minnecoder <sdparkin@protonmail.com>
2021-02-27 20:27:42 -0600

    Removed build folder from gitignore

---
+++ client/build/precache-manifest.3652855e7c143efc1f70a923b3cdc9cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7017ceaf55e359e414ea3dc7cfc5da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fb5e0cade83cd73b761",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "44f40a01100597e10cb5",
+    "url": "/static/js/2.c8c16e83.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c8c16e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb5e0cade83cd73b761",
+    "url": "/static/js/main.546eddf4.chunk.js"
+  },
+  {
+    "revision": "27e6be6cc426f6bd17ca",
+    "url": "/static/js/runtime-main.65325c0b.js"
+  }
+]);
