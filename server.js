d34e7b615971c119d24ea2595d7e2632514c9dad
minnecoder <sdparkin@protonmail.com>
2021-03-01 11:24:34 -0600

    Changed client build location

--- server.js
+++ server.js
@@ -26,10 +26,11 @@ const user = require("./routes/user");
 app.use("/api/v1/notes", notes);
 app.use("/api/v1/user", user);
 
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "./client/build/index.html"))
   );
 }
 
