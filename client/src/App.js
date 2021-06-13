504818c62208bc2e3fb36eb5efa86ad8e335b61c
minnecoder <sdparkin@protonmail.com>
2021-06-12 20:15:49 -0500

    Added global style

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components"
 import Login from "./components/Login";
 import StopList from "./components/StopList";
 import AddNote from "./components/AddNote";
@@ -7,17 +8,28 @@ import UpdateNote from "./components/UpdateNote";
 import Register from "./components/Register";
 import Demo from "./components/Demo";
 
+const GlobalStyle = createGlobalStyle`
+html {
+  margin: 0;
+  padding: 0;
+  background: #C0C6C8;
+}
+`
+
 export default function App() {
   return (
-    <HashRouter>
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/stops" component={StopList} />
-        <Route path="/add" exact component={AddNote} />
-        <Route path="/update" exact component={UpdateNote} />
-        <Route path="/register" exact component={Register} />
-        <Route path="/demo" exact component={Demo} />
-      </Switch>
-    </HashRouter>
+    <>
+      <GlobalStyle />
+      <HashRouter>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/stops" component={StopList} />
+          <Route path="/add" exact component={AddNote} />
+          <Route path="/update" exact component={UpdateNote} />
+          <Route path="/register" exact component={Register} />
+          <Route path="/demo" exact component={Demo} />
+        </Switch>
+      </HashRouter>
+    </>
   );
 }
