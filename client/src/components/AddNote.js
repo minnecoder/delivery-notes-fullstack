f2cc1e5d47ca86d0c13aaf81df21a30345814a16
minnecoder <sdparkin@protonmail.com>
2021-06-12 20:17:21 -0500

    Fixed issues with handleChange and handleChange

--- client/src/components/AddNote.js
+++ client/src/components/AddNote.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router"
 import styled from "styled-components";
 import NavBar from "./NavBar";
 
 export default function AddNote() {
+  const history = useHistory()
   const [stop, setStop] = useState({
     custName: "",
     address: "",
@@ -10,17 +12,16 @@ export default function AddNote() {
     city: "",
     deliveryLocation: "",
     notes: "",
-    error: "",
+    error: undefined,
   })
 
-  function handleChange(e) {
-    setStop.error(undefined);
-    setStop([e.target.name], e.target.value);
-    e.preventDefault();
+  const handleChange = event => {
+    event.persist();
+    setStop(prevStop => ({ ...prevStop, [event.target.name]: event.target.value }))
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  const handleSubmit = event => {
+    event.persist();
     const token = localStorage.getItem("token");
     fetch("/api/v1/notes", {
       method: "POST",
@@ -46,11 +47,11 @@ export default function AddNote() {
       signers: "",
       error: "",
     });
-    this.props.history.push("/stops");
+    history.push("/stops");
   }
 
   return (
-    <div>
+    <Main>
       <NavBar />
       <AddTitle>Add Stop</AddTitle>
       <p>{stop.error}</p>
@@ -60,55 +61,59 @@ export default function AddNote() {
           type="text"
           placeholder="Customer Name"
           value={stop.custName}
-          onChange={handleChange()}
+          onChange={handleChange}
         />
         <input
           name="address"
           type="text"
           placeholder="Address"
           value={stop.address}
-          onChange={handleChange()}
+          onChange={handleChange}
         />
         <input
           name="suite"
           type="text"
           placeholder="Suite"
           value={stop.suite}
-          onChange={handleChange()}
+          onChange={handleChange}
         />
         <input
           name="city"
           type="text"
           placeholder="City"
           value={stop.city}
-          onChange={handleChange()}
+          onChange={handleChange}
         />
         <input
           name="deliveryLocation"
           type="text"
           placeholder="Delivery Location"
           value={stop.deliveryLocation}
-          onChange={handleChange()}
+          onChange={handleChange}
         />
         <input
           name="notes"
           type="text"
           placeholder="Notes"
           value={stop.notes}
-          onChange={handleChange()}
+          onChange={handleChange}
         />
         <input
           name="signers"
           type="text"
           placeholder="Signers"
           value={stop.signers}
-          onChange={handleChange()}
+          onChange={handleChange}
         />
-        <input type="submit" value="Add Note" onClick={handleSubmit()} />
+        <input type="submit" value="Add Note" onClick={handleSubmit} />
       </AddForm>
-    </div>
+    </Main>
   )
 }
+
+const Main = styled.div`
+background: #C0C6C8;
+`
 
 const AddTitle = styled.h1`
   font-size: 3rem;
@@ -121,10 +126,25 @@ const AddForm = styled.form`
   display: flex;
   justify-content: center;
   flex-direction: column;
+  
+
 
   input[type="text"] {
     margin: 0.5rem 0;
     padding: 1rem 0;
     width: 50%;
+    border-radius: 8px;
+    border: solid 1px #C0C6C8;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+  }
+
+  input[type="submit"] {
+    background: #767676;
+    width: 5rem;
+    padding: 1rem 0;
+    margin-top: .5rem;
+    border: solid 1px #767676;
+    border-radius: 8px;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
   }
 `;
